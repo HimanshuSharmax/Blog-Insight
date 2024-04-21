aea4e81776e1eb2b31df3e0fe6e8285a1fd86599
HimanshuSharmax <ihimanshusharmax@gmail.com>
2024-04-21 18:30:53 +0530

    initial commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,65 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+     client = new Client();
+     account;
+
+     constructor(){
+          this.client
+               .setEndpoint(config.appwriteEndpoints)
+               .setProject(config.appwriteProjectId);
+          this.account = new Account(this.client);
+     }
+
+     async createAccount({email,password,name}){
+         try {
+               const userAccount = await this.account.create(ID.unique(),email,password,name);
+
+               if(userAccount){
+                    // call method
+                    alert("Successfully Account Created!")
+                    return this.login({email,password});
+               }else{
+                    return userAccount;
+               }
+
+         } catch (error) {
+               throw error;
+         } 
+     } 
+
+     async login({email, password}) {
+          try {
+               alert("Account Login!");
+              return await this.account.createEmailPasswordSession(email, password);
+          } catch (error) {
+              throw error;
+          //     console.log("Appwrite service :: login :: error",error);
+          }
+      }
+
+     async getCurrentUser(){
+          try {
+              return await this.account.get(); 
+          } catch (error) {
+               // throw error;
+               console.log("Appwrite service :: getCurrentUser :: error",error);
+          }
+          return null;
+     }
+
+     async logout(){
+          try {
+               alert("Are you sure? Logout!");
+              return await this.account.deleteSessions(); 
+          } catch (error) {
+               console.log("Appwrite service :: logout :: error",error);
+          }
+     }
+}
+
+const authService = new AuthService();
+
+export default authService
+
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <>
+      <img src={'https://stackideas.com/images/apps/2429/logo.png'} width = '60px' alt="BigCo Inc. logo"/>
+    </>
+  )
+}
+
+export default Logo
